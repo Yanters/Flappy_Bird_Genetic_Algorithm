a61b68cfeeb28d487fbbaf6e9fd3523f78c84f37
Yanters <yanter@wp.pl>
2023-05-01 22:40:14 +0200

    starting files

---
+++ config.py
@@ -0,0 +1,7 @@
+import pygame
+
+# pygame setup
+win_width = 1280
+win_height = 720
+window = pygame.display.set_mode((win_width, win_height))
+
---
+++ main.py
@@ -0,0 +1,43 @@
+import pygame
+import config
+
+
+# pygame setup
+pygame.init()
+clock = pygame.time.Clock()
+running = True
+dt = 0
+
+player_pos = pygame.Vector2(config.window.get_width() / 2, config.window.get_height() / 2)
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    config.window.fill("blue")
+
+    pygame.draw.circle(config.window, "red", player_pos, 40)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
